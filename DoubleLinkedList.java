0d4ccb9cddcc9cf764cd6e07de337380e34f9e2e
hendrmat <hendrmat@mail.gvsu.edu>
2019-04-06 17:17:30 -0400

    Added method to link broken elements together

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -51,16 +51,65 @@ public class DoubleLinkedList<E>  {
 	        this.top.setNext(newNode);
         }
 	    //If the size of the double linked list is > 1,
-		//Moves "curser" to the last element in the list and
-		//attaches "newNode" to "curser"
+		//Moves "cursor" to the last element in the list and
+		//attaches "newNode" to "cursor"
 	    else {
-	        for(int i = 0; i < this.size(); i++) {
-	            cursor = cursor.getNext();
-            }
+	        setCursor(this.size() - 1);
 	        newNode.setPrev(this.cursor);
 	        cursor.setNext(newNode);
         }
     }
+
+	/**
+	 * This will remove a given element from the doubly linked list at position index
+	 *
+	 * @param index The location of the element to remove
+	 * @return E For the element that is contained in the removed node
+	 */
+	public E remove(int index) {
+		//If the array size is 0, do nothing
+		if (this.size() == 0) {
+			return null;
+		}
+		//Set the element to where the cursor is
+		setCursor(index);
+		E element = cursor.getData();
+
+		//Sets the values of the temporary nodes
+		NodeD<E> tempPrevious = new NodeD<E>();
+		if (cursor.getPrev() != null) {
+			tempPrevious = cursor.getPrev();
+		}
+		NodeD<E> tempNext = new NodeD<E>();
+		if (cursor.getNext() != null) {
+			tempNext = cursor.getNext();
+		}
+
+		//Removes the first element and sets the previous to null
+		if (index == 0) {
+			this.top = tempNext;
+			tempNext.setPrev(null);
+		}
+
+		//Removes the element and attaches the elements before and after to one another
+		tempPrevious.setNext(tempNext);
+		tempNext.setPrev(tempPrevious);
+		cursor = this.top;
+
+		return element;
+	}
+
+	/**
+	 * Private helper method to loop through the message and set the index as directed
+	 *
+	 * @param index Where the cursor is focused
+	 */
+	private void setCursor(int index) {
+		cursor = this.top;
+		for (int i = 0; i <= index; i++) {
+			cursor = cursor.getNext();
+		}
+	}
 
     /**
      * Method to increment the size of a doubly linked list
