92946e35b8ecb603a244bf9ead774be6bd256eb5
bomerst <bomerst@mail.gvsu.edu>
2019-04-12 01:53:28 -0400

    Fixed bug with InsertBefore for real again again

--- UnMix.java
+++ UnMix.java
@@ -56,7 +56,7 @@ public class UnMix {
 	private void insertbefore(int index, String token) {
 		for (int i = token.length() - 1; i >= 0; i--) {
             if (token.charAt(i) == '~') {
-                message.add(' ');
+                message.add(index, ' ');
             } else {
                 message.add(index, token.charAt(i));
             }
